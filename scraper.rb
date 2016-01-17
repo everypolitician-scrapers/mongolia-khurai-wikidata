e3783442574b7cc580142340bbf7aa9202476d96
Tony Bowden <tony@tmtm.com>
2016-01-17 10:12:28 +0000

    English names

--- scraper.rb
+++ scraper.rb
@@ -8,6 +8,13 @@ mn_names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://mn.wikipedia.org/wiki/Загвар:2012-2016_оны_УИХ-ын_гишүүд',
   xpath: '//table[@class="navbox"]//table//tr[td]//td[1]//a[not(@class="new")]/@title',
 ) 
-EveryPolitician::Wikidata.scrape_wikidata(names: { mn: names })
+
+en_names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/List_of_MPs_elected_in_the_Mongolian_legislative_election,_2012',
+  after: '//span[@id="Party_list"]',
+  xpath: '//table//td[1]//a[not(@class="new")]/@title',
+) 
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { mn: mn_names, en: en_naames })
 warn EveryPolitician::Wikidata.notify_rebuilder
 
