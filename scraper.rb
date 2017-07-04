76ca336ebf418499d83dede8680a1f7f2ce8faa0
Tony Bowden <tony@tmtm.com>
2017-07-04 10:29:53 +0100

    Also fetch via SPARQL

--- scraper.rb
+++ scraper.rb
@@ -7,8 +7,16 @@ require 'pry'
 mn_names = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://mn.wikipedia.org/wiki/Загвар:2012-2016_оны_УИХ-ын_гишүүд',
   xpath: '//table[@class="navbox"]//table//tr[td]//td[1]//a[not(@class="new")]/@title',
-) 
+)
 
 en_names = EveryPolitician::Wikidata.morph_wikinames(source: 'everypolitician-scrapers/mongolia-khurai-wp-multiple-terms', column: 'wikiname')
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { mn: mn_names, en: en_names })
+sparq = <<EOQ
+  SELECT ?item WHERE {
+    VALUES ?term { wd:Q28112313 wd:Q28112515 wd:Q28112522 }
+    ?item p:P39 [ ps:P39 wd:Q21328637 ; pq:P2937 ?term ]
+  }
+EOQ
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { mn: mn_names, en: en_names })
